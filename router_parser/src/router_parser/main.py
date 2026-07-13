7232b717b2acb5cc80ef4068a6a59abba7193e50
Kirtan Dave <kirtand88@gmail.com>
2026-07-13 16:35:50 +0530

    fix: run flake8 and black commands to check code formatting and quality

--- router_parser/src/router_parser/main.py
+++ router_parser/src/router_parser/main.py
@@ -1,15 +1,14 @@
+from router_parser.models import Router
+from router_parser.utils import process_csv
 import json
 from pathlib import Path
 
 import typer
 import colorama
-from colorama import Fore, Style
 colorama.just_fix_windows_console()
 
-from router_parser.models import Router
-from router_parser.utils import process_csv
-
 app = typer.Typer()
+
 
 @app.command()
 def export(
@@ -27,22 +26,25 @@ def export(
             hostname=row["hostname"],
             ip_address=row["ip_address"],
             model=row["model"],
-            site=row["site"]
+            site=row["site"],
         )
         store_router_objects.append(router_instance)
-    
+
     output_data = []
     for router in store_router_objects:
-        output_data.append({
-            "hostname": router.hostname,
-            "ip_address": router.ip_address,
-            "model": router.model,
-            "site": router.site
-        })
+        output_data.append(
+            {
+                "hostname": router.hostname,
+                "ip_address": router.ip_address,
+                "model": router.model,
+                "site": router.site,
+            }
+        )
 
     output_file.parent.mkdir(parents=True, exist_ok=True)
-    with open(output_file, mode='w', encoding='utf-8') as json_file:
+    with open(output_file, mode="w", encoding="utf-8") as json_file:
         json.dump(output_data, json_file, indent=2)
+
 
 if __name__ == "__main__":
     app()
